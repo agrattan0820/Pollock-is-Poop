0320ed71d43f473ed952c5fcde687a7c283b3813
GameDog9988 <alex@grattan.me>
2021-12-28 10:20:44 -0500

    feat: damn

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        damn: ["Damn", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
