1aa58bdad5a63c4ac6b87b77d822f3b24f16e8df
GameDog9988 <alex@grattan.me>
2021-12-28 16:09:15 -0500

    feat: image upload and font optimization

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preload" href="/fonts/DAMN.ttf" as="font" crossOrigin="" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       fontFamily: {
         damn: ["Damn", "sans-serif"],
+        space: ["Space Mono", "monospace"],
       },
     },
   },
